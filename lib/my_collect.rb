603a988a71edb3d8026bdf6266ea11b4403655dc
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-31 23:34:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,17 @@
-language = ["ruby","javascript","python","objective-c"]
+array = ["ruby","javascript","python","objective-c"]
 
-def my_collect(arr)
+
+
+
+def my_collect(array)
+  counter = 0
+  collection = []
   
-
+  while counter < array.length
+    collection << yield(array[counter])
+    counter += 1
+  end
+  collection
 end
+
+my_collect(array){|i|"#{i}"}
